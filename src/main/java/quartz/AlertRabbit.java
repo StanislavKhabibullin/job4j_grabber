72d8eb5630244a5ff2559f7f4f21c2fc7030185d
Stanislav <skhabibullin@yandex.ru>
2021-06-28 21:54:33 +0300

    1. Quartz [#175122]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -4,7 +4,11 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -13,16 +17,46 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
 
-    private static String pathtoproperties = "C:\\projects\\job4j_grabber\\src\\main\\resources\\rabbit.properties";
+    private static String pathtoproperties
+            = "C:\\projects\\job4j_grabber\\src\\main\\resources\\rabbit.properties";
+    private static Connection connect;
+    Integer seq;
 
-    public static void main(String[] args) throws IOException {
-        Properties prop = new Properties();
+    private Integer init() throws IOException, SQLException, ClassNotFoundException {
+
         try (FileInputStream in = new FileInputStream(pathtoproperties)) {
+            Properties prop = new Properties();
             prop.load(in);
-            int requestTime = Integer.valueOf(prop.getProperty("rabbit.interval"));
+            seq = Integer.valueOf(prop.getProperty("rabbit.interval"));
+            Class.forName(prop.getProperty("driver-class-name"));
+            connect = DriverManager.getConnection(
+                    prop.getProperty("url"),
+                    prop.getProperty("username"),
+                    prop.getProperty("password")
+            );
+        }
+        return seq;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException, SchedulerException, IOException {
+            AlertRabbit rabbit = new AlertRabbit();
+        int requestTime = rabbit.init();
+            List<Long> store = new ArrayList<>();
+            try (Statement statement = connect.createStatement()) {
+                String sql = String.format(
+                        "Create table if not exists "
+                                + "rabbit"
+                                + "(id serial primary key, created_date DATE);");
+                statement.execute(sql);
+                System.out.println(Rabbit.getTableScheme(connect, "rabbit"));
+            }
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("store", store);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(requestTime)
                     .repeatForever();
@@ -31,15 +65,40 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+            Thread.sleep(5000);
+            scheduler.shutdown();
+            System.out.println(store);
         }
     }
 
-    public static class Rabbit implements Job {
+    class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+            store.add(System.currentTimeMillis());
+        }
+
+        public static String getTableScheme(Connection connection, String tableName) throws SQLException {
+            StringBuilder scheme = new StringBuilder();
+            DatabaseMetaData metaData = connection.getMetaData();
+            try (ResultSet columns = metaData.getColumns(null, null,
+                    tableName, null)) {
+                scheme.append(String.format("%-15s %-15s%n", "column", "type"));
+                while (columns.next()) {
+                    scheme.append(String.format("%-15s %-15s%n",
+                            columns.getString("COLUMN_NAME"),
+                            columns.getString("TYPE_NAME")));
+                }
+            }
+            return scheme.toString();
         }
     }
-}
+
+
+
